9dc549bf5a19ddea770a01edcc4f960e0774079f
Jan Kowalczuk <jankow97@gmail.com>
2020-03-02 01:22:00 +0100

    Fix data reading

--- GraphSearchApp/GraphSearchApp/IO/ReadStandardData.cs
+++ GraphSearchApp/GraphSearchApp/IO/ReadStandardData.cs
@@ -17,26 +17,48 @@ namespace GraphSearchApp.IO
                 AdjacencyList = new List<List<Connection>>()
             };
 
-            string[] data = dataToRead.Split(new char[] { '\n' });
-            foreach (var line in data)
+            string[] data = dataToRead.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+            string[] singleNumbers = data[0].Split(new char[] { ' ' });
+
+            int.TryParse((string)singleNumbers[0], out var numberOfCities);
+            for (int i = 0; i < numberOfCities; i++)
             {
-                string[] singleStrings = line.Split(new char[] { ' ' });
                 graph.AdjacencyList.Add(new List<Connection>());
-                try
-                {
+            }
+
+            int.TryParse(singleNumbers[1], out var numberOfConnections);
+
+            for(int i = 1; i < 1 + numberOfConnections; i++)
+            {
+                string line = data[i];
+                string[] singleStrings = line.Split(new char[] { ' ' });
+                //try
+                //{
                     Connection connection = new Connection()
                     {
                         CityA = int.Parse(singleStrings[0]),
                         CityB = int.Parse(singleStrings[1]),
                         Distance = int.Parse(singleStrings[2])
                     };
-                    graph.AdjacencyList[graph.AdjacencyList.Capacity - 1].Add(connection);
-                }
-                catch (Exception e)
-                {
-                    throw; // todo
-                }
+                    graph.AdjacencyList[connection.CityA - 1].Add(connection);
+                    connection = new Connection()
+                    {
+                        CityA = int.Parse(singleStrings[1]),
+                        CityB = int.Parse(singleStrings[0]),
+                        Distance = int.Parse(singleStrings[2])
+                    };
+                    graph.AdjacencyList[connection.CityA - 1].Add(connection); //CityA is now different
+                //}
+                //catch (Exception e)
+                //{
+                //    throw; // todo
+                //}
             }
+
+            string[] road = data[1 + numberOfConnections].Split(new char[] { ' ' }); // todo: finish
+            //road[0] is a starting node
+            //road[1] is an ending node
 
             return graph;
         }
